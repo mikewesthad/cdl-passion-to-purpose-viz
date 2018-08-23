7024cb8a3520536ca389055fb3dacd18ac5cd940
Michael Hadley <michaelwesthadley@gmail.com>
2018-08-23 11:51:16 -0500

    Add timestamp to responses page

--- src/pages/responses.js
+++ src/pages/responses.js
@@ -3,6 +3,7 @@ import { observer } from "mobx-react";
 import { passionPrompts, purposePrompts } from "../data";
 import { Grid, Column2 } from "../components/grid";
 import Container from "../components/container";
+import format from "date-fns/format";
 
 @observer
 export default class Responses extends Component {
@@ -14,66 +15,80 @@ export default class Responses extends Component {
     return (
       <Container>
         {entries.map(([key, value], i) => {
-          const { passions, purposes } = value;
+          const { passions, purposes, timestamp } = value;
           return (
-            <Grid key={key}>
-              <Column2>
-                <div
-                  style={{
-                    fontWeight: "bold",
-                    marginBottom: "0.5rem",
-                    color: "#0093ff"
-                  }}
-                >
-                  Passions
-                </div>
+            <div style={{ maxWidth: "700px", margin: "0 auto 1.5rem auto" }} key={key}>
+              <div
+                style={{
+                  fontWeight: "300",
+                  fontSize: "0.6rem",
+                  fontStyle: "italic",
+                  margin: "0 0 0.3rem"
+                }}
+              >
+                {format(timestamp, "[On] MM/DD/YYYY [at] hh:mma[:]")}
+              </div>
+              <Grid>
+                <Column2>
+                  <div
+                    style={{
+                      fontWeight: "bold",
+                      marginBottom: "0.5rem",
+                      color: "#0093ff"
+                    }}
+                  >
+                    Passions
+                  </div>
 
-                <ul style={{ lineHeight: "1.1rem" }}>
-                  {passions.map((passion, i) => (
-                    <li style={{ marginBottom: "0.5rem" }} key={`passion-${key}-${i}`}>
-                      <div
-                        style={{
-                          fontSize: "0.75rem",
-                          fontWeight: 300,
-                          fontStyle: "italic"
-                        }}
-                      >
-                        {passionPrompts[i]}...
-                      </div>
-                      <div>{passion}</div>
-                    </li>
-                  ))}
-                </ul>
-              </Column2>
+                  <ul style={{ lineHeight: "1.1rem" }}>
+                    {passions.map((passion, i) => (
+                      <li style={{ marginBottom: "0.5rem" }} key={`passion-${key}-${i}`}>
+                        <div
+                          style={{
+                            fontSize: "0.75rem",
+                            fontWeight: 300,
+                            fontStyle: "italic"
+                          }}
+                        >
+                          {passionPrompts[i]}
+                          ...
+                        </div>
+                        <div>{passion}</div>
+                      </li>
+                    ))}
+                  </ul>
+                </Column2>
 
-              <Column2>
-                <div
-                  style={{
-                    fontWeight: "bold",
-                    marginBottom: "0.5rem",
-                    color: "#fd940d"
-                  }}
-                >
-                  Purposes
-                </div>
-                <ul style={{ lineHeight: "1.1rem" }}>
-                  {purposes.map((purpose, i) => (
-                    <li style={{ marginBottom: "0.5rem" }} key={`purpose-${key}-${i}`}>
-                      <div
-                        style={{
-                          fontSize: "0.75rem",
-                          fontWeight: 300,
-                          fontStyle: "italic"
-                        }}
-                      >
-                        {purposePrompts[i]}...
-                      </div>
-                      <div>{purpose}</div>
-                    </li>
-                  ))}
-                </ul>
-              </Column2>
-            </Grid>
+                <Column2>
+                  <div
+                    style={{
+                      fontWeight: "bold",
+                      marginBottom: "0.5rem",
+                      color: "#fd940d"
+                    }}
+                  >
+                    Purposes
+                  </div>
+                  <ul style={{ lineHeight: "1.1rem" }}>
+                    {purposes.map((purpose, i) => (
+                      <li style={{ marginBottom: "0.5rem" }} key={`purpose-${key}-${i}`}>
+                        <div
+                          style={{
+                            fontSize: "0.75rem",
+                            fontWeight: 300,
+                            fontStyle: "italic"
+                          }}
+                        >
+                          {purposePrompts[i]}
+                          ...
+                        </div>
+                        <div>{purpose}</div>
+                      </li>
+                    ))}
+                  </ul>
+                </Column2>
+              </Grid>
+            </div>
           );
         })}
       </Container>
