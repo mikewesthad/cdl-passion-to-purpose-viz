3247d13e14734abebb5e2277d21a63df76f5b75b
Michael Hadley <michaelwesthadley@gmail.com>
2018-08-24 13:50:12 -0500

    Add temporary filtering capabilities. Todo: move this to a proper admin UI

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import firebase from "../utils/firebase";
 import { observable } from "mobx";
 import { generateRepresentativeCombos } from "../utils/array-utils";
 import { purposeVerbs } from "../data";
+// import isWithinRange from "date-fns/is_within_range";
+// import youthResponses from "./passion-to-purpose-v2-export.json";
 
 const database = firebase.database();
 
@@ -16,6 +18,21 @@ class Store {
 
   onFirebaseValue = snapshot => {
     this.data = snapshot.val() || {}; // Empty db is null
+
+    // Inject the local copy of youth responses into the viz
+    // Object.assign(this.data, youthResponses);
+
+    // Filter only specific data
+    // const val = snapshot.val();
+    // const filteredData = {};
+    // const startDate = new Date("August 22, 2018 00:00:00");
+    // const endDate = new Date("August 22, 2018 17:00:00");
+    // Object.entries(val).forEach(([key, value]) => {
+    //   const { timestamp } = value;
+    //   if (isWithinRange(timestamp, startDate, endDate)) filteredData[key] = value;
+    // });
+    // this.data = filteredData;
+
     this.hasLoaded = true;
 
     const permutations = [];
