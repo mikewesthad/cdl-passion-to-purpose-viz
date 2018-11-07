01543bc4f6f59489813d1777158d00c42435ffe6
Michael Hadley <michaelwesthadley@gmail.com>
2018-11-07 13:35:58 -0600

    Remove CRA service worker boilerplate

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import "./style/index.scss";
 import "typeface-montserrat";
-// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById("root"));
-// registerServiceWorker();
