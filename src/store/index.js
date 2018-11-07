c5b017f20c9e40a4e4b54043cae9a02683fcdd08
Michael Hadley <michaelwesthadley@gmail.com>
2018-11-07 16:05:11 -0600

    Update store to pool all passions and purposes into arrays

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,22 @@
 import firebase from "../utils/firebase";
-import { observable } from "mobx";
+import { observable, computed, toJS } from "mobx";
 import { generateRepresentativeCombos } from "../utils/array-utils";
-import { purposeVerbs } from "../data";
+import { purposeVerbs, passionPrompts, purposePrompts } from "../data";
 // import isWithinRange from "date-fns/is_within_range";
 // import youthResponses from "./passion-to-purpose-v2-export.json";
 
 const database = firebase.database();
 
+const NUM_PASSIONS = passionPrompts.length;
+const NUM_PURPOSES = purposePrompts.length;
+
 class Store {
-  /*prettier-ignore*/ @observable data = null;
-  /*prettier-ignore*/ @observable hasLoaded = false;
-  /*prettier-ignore*/ @observable permutations = null;
+  @observable data = null;
+  @observable numResponses = 0;
+  @observable allPurposes = [];
+  @observable allPassions = [];
+  @observable hasLoaded = false;
+  @observable permutations = null;
 
   constructor() {
     database.ref().on("value", this.onFirebaseValue);
@@ -35,14 +41,26 @@ class Store {
 
     this.hasLoaded = true;
 
+    const values = Object.values(this.data);
+    const allPassions = [...Array(NUM_PASSIONS)].map(() => []);
+    const allPurposes = [...Array(NUM_PURPOSES)].map(() => []);
     const permutations = [];
     const seed = 2;
-    Object.values(this.data).forEach(({ passions, purposes }, i) => {
+    values.forEach(({ passions, purposes }, i) => {
       const purposesWithVerbs = purposes.map((purpose, i) => `${purposeVerbs[i]} ${purpose}`);
       permutations.push(...generateRepresentativeCombos(passions, purposesWithVerbs, 3, seed));
+
+      // Most recent responses first
+      passions.forEach((p, i) => allPassions[i].unshift(p));
+      purposes.forEach((p, i) => allPurposes[i].unshift(p));
     });
     permutations.reverse();
     this.permutations = permutations;
+
+    this.allPassions = allPassions;
+    this.allPurposes = allPurposes;
+
+    this.numResponses = values.length;
   };
 }
 
